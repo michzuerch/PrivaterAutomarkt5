a9f7648fbebbc6deabd88735a70d83057c3908cb
Michael Zuercher <michzuerch@gmail.com>
2022-10-13 11:25:09 +0200

    remove seo, analytics, new versions on package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
 import mdx from "@astrojs/mdx";
 
