1e8d15248ddb67d5d73b59cdb918aeb1c2fbb848
Michael Zuercher <michzuerch@gmail.com>
2022-10-14 13:58:22 +0200

    Problem with prettier and tailwindccs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,39 +4,39 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended", "prettier"],
+  extends: ['eslint:recommended', 'plugin:astro/recommended'],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {},
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
       },
       rules: {},
     },
     {
-      files: ["*.ts"],
-      parser: "@typescript-eslint/parser",
-      extends: ["plugin:@typescript-eslint/recommended"],
+      files: ['*.ts'],
+      parser: '@typescript-eslint/parser',
+      extends: ['plugin:@typescript-eslint/recommended'],
       rules: {
-        "@typescript-eslint/no-unused-vars": [
-          "error",
-          { argsIgnorePattern: "^_", destructuredArrayIgnorePattern: "^_" },
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' },
         ],
-        "@typescript-eslint/no-non-null-assertion": "off",
+        '@typescript-eslint/no-non-null-assertion': 'off',
       },
     },
     {
       // Define the configuration for `<script>` tag.
       // Script in `<script>` is assigned a virtual file name with the `.js` extension.
-      files: ["**/*.astro/*.js", "*.astro/*.js"],
-      parser: "@typescript-eslint/parser",
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      parser: '@typescript-eslint/parser',
     },
   ],
 };
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,22 +1,22 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-const colors = require("tailwindcss/colors");
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 
 module.exports = {
-	mode: "jit",
-	darkMode: "class",
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	mode: 'jit',
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	options: {
-		safelist: ["dark"],
+		safelist: ['dark'],
 	},
 	theme: {
 		// theme extensions
 		extend: {},
 	},
 	plugins: [
-		require("@tailwindcss/aspect-ratio"),
-		require("@tailwindcss/typography"),
-		require("@tailwindcss/line-clamp"),
-		require("@tailwindcss/forms"),
-		require("daisyui"),
+		require('@tailwindcss/aspect-ratio'),
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/forms'),
+		require('daisyui'),
 	],
 };
