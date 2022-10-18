a2499f53f4cc75b144fc204769d4d1dd65bcb0ad
Michael Zuercher <michzuerch@gmail.com>
2022-10-18 15:36:58 +0200

    styled map, on the way to prettier tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import svelte from '@astrojs/svelte';
-import image from '@astrojs/image';
-import alpinejs from '@astrojs/alpinejs';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import svelte from "@astrojs/svelte";
+import image from "@astrojs/image";
+import alpinejs from "@astrojs/alpinejs";
 
 export default defineConfig({
 	// @todo  Change to productive environment
-	site: 'https://michzuerch.github.io/',
-	base: '/PrivaterAutomarkt5',
+	site: "https://michzuerch.github.io/",
+	base: "/PrivaterAutomarkt5",
 	build: {
-		format: 'file',
+		format: "file",
 	},
 	integrations: [tailwind(), mdx(), sitemap(), svelte(), image(), alpinejs()],
 });
